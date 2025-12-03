47226bcbc9f256807cf39c5079689a5a35f69892
lavalex <lavrov.alex@gmail.com>
2025-12-03 10:37:40 +0000

    change

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const http = require("http");
 const port = 8080;
 http.createServer((req, res) => {
-  res.end("Hello from OpenShift! Fuck Yaron :)\n");
+  res.end("Hello from OpenShift! Fuck Yaron and Nir)\n");
 }).listen(port, () => console.log(`Listening on ${port}`));
 
